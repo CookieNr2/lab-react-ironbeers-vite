3d347dcfe9feb59e5d9c62683040a1222fab8a4f
Oksana Klochak <oksana.klochak@gmail.com>
2024-04-21 18:57:20 +0200

    iteration 1 done. lacks styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import BeerDetailsPage from "./pages/BeerDetailsPage";
+import Home from "./pages/HomePage";
+import AllBeersPage from "./pages/AllBeersPage";
+import RandomBeerPage from "./pages/RandomBeerPage";
+import AddBeerPage from "./pages/AddBeerPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>LAB | React IronBeers</h1>
+    <div>
+      <div className="App">
+        <h1>LAB | React IronBeers</h1>
+      </div>
+      <Routes>
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="*" element={<Navigate to="/home" />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,19 @@
-function HomePage() {}
+import { Routes, Route, Navigate, Link, useNavigate } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <ul>
+      <li>
+        <Link to="/beers">All Beers</Link>
+      </li>
+      <li>
+        <Link to="/random-beer">Random Beer</Link>
+      </li>
+      <li>
+        <Link to="/new-beer">New Beer</Link>
+      </li>
+    </ul>
+  );
+}
 
 export default HomePage;
