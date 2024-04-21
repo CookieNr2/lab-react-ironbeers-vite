0fcfa4589578548fd5c3d489c2e6b3c1fd1d74d9
Oksana Klochak <oksana.klochak@gmail.com>
2024-04-21 19:20:21 +0200

    iteration 4 done. Missing data and styling

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,21 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function AllBeersPage() {
+  const [beers, setBeers] = useState();
+  useEffect(() => {
+    axios.get("https://ih-beers-api2.herokuapp.com/beers").then((res) => {
+      setBeers(res.data);
+    });
+  }, []);
+  if (!beers) return <p>Loading...</p>;
+  return (
+    <ul>
+      {beers.map((element) => {
+        return <li key={element._id}>{element.name}</li>;
+      })}
+    </ul>
+  );
+}
 
 export default AllBeersPage;
